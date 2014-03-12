15ad55d5db4c7ed90a35fda89a98e11f5b32063d
Igor Vayner <igor.vayner@mlb.com>
2014-03-12 21:07:53 +0000

    AT:second part of HttpService with Helper

--- mlb-qa/src/main/java/com/mlb/qa/android/atb/service/http/AtbHttpService.java
+++ mlb-qa/src/main/java/com/mlb/qa/android/atb/service/http/AtbHttpService.java
@@ -8,7 +8,6 @@ import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 
 import com.mlb.qa.android.atb.utils.AtbParameter;
-import com.mlb.qa.exception.TestRuntimeException;
 
 
 public class AtbHttpService {
@@ -18,10 +17,11 @@ public class AtbHttpService {
 	private static final String KEY_PARAM = "key";
 	private static final String VALUE_PARAM = "value";
 	private static final String PROPERTY_UPDATED_RESPONSE_REGEX = ".*%s.*property is updated to.*s.*";
-
+	private static final String GAME_FEED_URL_PATTERN = "http://gd.qa.mlb.com/components/game/mlb/year_%4d/month_%02d/day_%02d/master_scoreboard.xml";
+	
 	public enum AtbCheckinProperty {
-		GAME_FEED("com.bamnetworks.mobile.service.social.game.GameService.gameFeedURL"), 
-		ALLOW_CHKECKIN_TIME("com.bamnetworks.mobile.service.social.game.GameService.timeBoundary"),
+		GAME_FEED("com.bamnetworks.mobile.service.instadium.game.GameService.gameFeedURL"), 
+		TIME_BOUNDARY("com.bamnetworks.mobile.service.social.game.GameService.timeBoundary"),
 		;
 		private String propertyName;
 
@@ -43,19 +43,19 @@ public class AtbHttpService {
 				timeInSeconds));
 		Map<String, String> bodyParameters = new HashMap<String, String>();
 		bodyParameters.put(KEY_PARAM,
-				AtbCheckinProperty.ALLOW_CHKECKIN_TIME.getPropertyName());
+				AtbCheckinProperty.TIME_BOUNDARY.getPropertyName());
 		bodyParameters.put(VALUE_PARAM, timeInSeconds.toString());
 		HttpResult result = HttpHelper.executePostMethod(
 				AtbParameter.MLB_ATB_SOCIAL_SERVICE_PROPERTY_HOST.getValue(),
 				new HashMap<String, String>(), bodyParameters, UTF_8_CHARSET);
 		HttpHelper.checkResult(result, HttpStatus.SC_OK, String.format(
 				PROPERTY_UPDATED_RESPONSE_REGEX,
-				AtbCheckinProperty.ALLOW_CHKECKIN_TIME.getPropertyName(),
+				AtbCheckinProperty.TIME_BOUNDARY.getPropertyName(),
 				timeInSeconds.toString()));
 	}
 
 	/**
-	 * Set GameFeed property of mobile-service-social (for the specified day)
+	 * Set GameFeed property of mobile-service-social (for the specified date)
 	 * 
 	 * @param year
 	 * @param month
@@ -63,8 +63,20 @@ public class AtbHttpService {
 	 */
 	public void setGameFeedCheckinServiceProperty(Integer year, Integer month,
 			Integer day) {
-		// TODO implement!
-		throw new TestRuntimeException("Not implemented yet");
+		logger.info(String.format("Allow checkin for the specified date. Year: %04d, Month: %02d, Day: %02d",
+				year, month, day));
+		Map<String, String> bodyParameters = new HashMap<String, String>();
+		bodyParameters.put(KEY_PARAM,
+				AtbCheckinProperty.GAME_FEED.getPropertyName());
+		String value = String.format(GAME_FEED_URL_PATTERN, year, month, day);
+		bodyParameters.put(VALUE_PARAM, value);
+		HttpResult result = HttpHelper.executePostMethod(
+				AtbParameter.MLB_ATB_SOCIAL_SERVICE_PROPERTY_HOST.getValue(),
+				new HashMap<String, String>(), bodyParameters, UTF_8_CHARSET);
+		HttpHelper.checkResult(result, HttpStatus.SC_OK, String.format(
+				PROPERTY_UPDATED_RESPONSE_REGEX,
+				AtbCheckinProperty.GAME_FEED.getPropertyName(),
+				value));
 	}
 
 	/**
