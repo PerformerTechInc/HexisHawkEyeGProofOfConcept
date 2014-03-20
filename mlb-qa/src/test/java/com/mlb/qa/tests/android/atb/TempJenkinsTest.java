3ab0a2f3035d4d658a4cef43975add6a57761872
Igor Vayner <igor.vayner@mlb.com>
2014-03-20 23:42:39 +0000

    AT: temporary empty test to check jenkins configuration

---
+++ mlb-qa/src/test/java/com/mlb/qa/tests/android/atb/TempJenkinsTest.java
@@ -0,0 +1,12 @@
+package com.mlb.qa.tests.android.atb;
+
+import org.testng.annotations.Test;
+
+import com.qaprosoft.carina.core.foundation.UITest;
+
+public class TempJenkinsTest extends UITest{
+	@Test(description = "Check jenkins config test")
+	public void checkJenkinsConfig(){
+		// nothing to do
+	}
+}
