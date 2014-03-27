2a25e2de3e7a2df4d8a16f91569d464de3ab9a28
Igor Vayner <igor.vayner@mlb.com>
2014-03-27 21:44:15 +0000

    AT: test for proxyAdd POST

--- mlb-qa/src/test/java/com/mlb/qa/tests/android/atb/CheckProxyTest.java
+++ mlb-qa/src/test/java/com/mlb/qa/tests/android/atb/CheckProxyTest.java
@@ -2,14 +2,22 @@ package com.mlb.qa.tests.android.atb;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 import org.apache.log4j.Logger;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -28,7 +36,7 @@ public class CheckProxyTest extends APITest {
        // System.setProperty("http.proxyPort", "80");
 	}
 	@Test()
-	public void checkProxyGet() {
+	public void checkProxyGet() throws ClientProtocolException, IOException {
 		/*String season = AtbParameter.MLB_ATB_SEASON.getValue();
 		Team team = lookupService.lookupTeamByAbbrev("ARI", season);
 		DateTime today = new DateTime();
@@ -42,17 +50,30 @@ public class CheckProxyTest extends APITest {
 		HttpGet get = new HttpGet("http://qa.mlb.com/lookup/named.team_all_season.bam?team_all_season.col_in=venue_id&team_all_season.col_in=name_display_full&team_all_season.col_in=team_id&team_all_season.col_in=name&team_all_season.col_in=name_short&team_all_season.col_in=name_abbrev&team_all_season.col_in=city&team_all_season.col_in=venue_name&name_abbrev='BAL'&season=2014&sport_id=1");
 		logger.info(get);
 		client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
-		try {
 			HttpResponse response = client.execute(get);
 		logger.info(response.toString());
-			InputStream istream = response.getEntity().getContent();
-			String result = IOUtils.toString(istream);
+			String result = IOUtils.toString(response.getEntity().getContent());
 			logger.info(result);
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+	}
+	
+	@Test()
+	public void checkPost() throws ClientProtocolException, IOException{
+		 HttpPost httpost = new HttpPost("http://qamsocial.bamnetworks.com/mobile-service-social/property");
+			HttpHost proxy = new HttpHost("proxy-vip.mm3.mlbam.com", 80);
+
+	        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+	        nvps.add(new BasicNameValuePair("key", "username"));
+	        nvps.add(new BasicNameValuePair("IDToken2", "password"));
+				httpost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+	
+	        DefaultHttpClient client = new DefaultHttpClient();
+	        client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+			logger.info(client);
+	        HttpResponse response = client.execute(httpost);
+	        logger.info(response);
+	    	String result = IOUtils.toString(response.getEntity().getContent());
+			logger.info(result);
 	}
 	
 }
