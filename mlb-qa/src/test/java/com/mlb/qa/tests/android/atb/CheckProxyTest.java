4caa4c7feb362ae05ec20fb94ca4fa5433007728
Igor Vayner <igor.vayner@mlb.com>
2014-03-27 21:17:32 +0000

    AT: test for proxy.Add logging

--- mlb-qa/src/test/java/com/mlb/qa/tests/android/atb/CheckProxyTest.java
+++ mlb-qa/src/test/java/com/mlb/qa/tests/android/atb/CheckProxyTest.java
@@ -12,6 +12,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.log4j.Logger;
 import org.apache.xmlbeans.impl.common.IOUtil;
 import org.joda.time.DateTime;
 import org.testng.annotations.BeforeClass;
@@ -27,7 +28,7 @@ import com.qaprosoft.carina.core.foundation.APITest;
 public class CheckProxyTest extends APITest {
 	private AtbLookupService lookupService = new AtbLookupService();
 	private AtbHttpService httpService = new AtbHttpService();
-
+	private static final Logger logger = Logger.getLogger(CheckProxyTest.class);
 	@BeforeClass()
 	public void setProxySettings(){
 		//System.setProperty("http.proxyHost", "http://proxy-vip.mm3.mlbam.com");
@@ -42,8 +43,11 @@ public class CheckProxyTest extends APITest {
 				team.getTeamId(), team.getVenueId(), today,
 				today.plusMonths(6), season);*/
 		HttpHost proxy = new HttpHost("http://proxy-vip.mm3.mlbam.com", 80);
+		logger.info(proxy);
 		DefaultHttpClient client = new DefaultHttpClient();
+		logger.info(client);
 		HttpGet get = new HttpGet("http://qa.mlb.com/lookup/named.team_all_season.bam?team_all_season.col_in=venue_id&team_all_season.col_in=name_display_full&team_all_season.col_in=team_id&team_all_season.col_in=name&team_all_season.col_in=name_short&team_all_season.col_in=name_abbrev&team_all_season.col_in=city&team_all_season.col_in=venue_name&name_abbrev='BAL'&season=2014&sport_id=1");
+		logger.info(get);
 		client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
 		try {
 			HttpResponse response = client.execute(get);
