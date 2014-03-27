5f546ebf90cc86dd2a0ef5f51b3736eb14320467
Igor Vayner <igor.vayner@mlb.com>
2014-03-27 21:11:34 +0000

    AT: test for proxy

--- mlb-qa/src/test/java/com/mlb/qa/tests/android/atb/CheckProxyTest.java
+++ mlb-qa/src/test/java/com/mlb/qa/tests/android/atb/CheckProxyTest.java
@@ -1,5 +1,18 @@
 package com.mlb.qa.tests.android.atb;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+import org.apache.commons.httpclient.HttpHost;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.xmlbeans.impl.common.IOUtil;
 import org.joda.time.DateTime;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,21 +30,40 @@ public class CheckProxyTest extends APITest {
 
 	@BeforeClass()
 	public void setProxySettings(){
-		System.setProperty("http.proxyHost", "http://proxy-vip.mm3.mlbam.com");
-        System.setProperty("http.proxyPort", "80");
+		//System.setProperty("http.proxyHost", "http://proxy-vip.mm3.mlbam.com");
+       // System.setProperty("http.proxyPort", "80");
 	}
 	@Test()
 	public void checkProxyGet() {
-		String season = AtbParameter.MLB_ATB_SEASON.getValue();
+		/*String season = AtbParameter.MLB_ATB_SEASON.getValue();
 		Team team = lookupService.lookupTeamByAbbrev("ARI", season);
 		DateTime today = new DateTime();
 		lookupService.lookupNearestIncomingHomeGame(
 				team.getTeamId(), team.getVenueId(), today,
-				today.plusMonths(6), season);
+				today.plusMonths(6), season);*/
+		HttpHost proxy = new HttpHost("http://proxy-vip.mm3.mlbam.com", 80);
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpGet get = new HttpGet("http://qa.mlb.com/lookup/named.team_all_season.bam?team_all_season.col_in=venue_id&team_all_season.col_in=name_display_full&team_all_season.col_in=team_id&team_all_season.col_in=name&team_all_season.col_in=name_short&team_all_season.col_in=name_abbrev&team_all_season.col_in=city&team_all_season.col_in=venue_name&name_abbrev='BAL'&season=2014&sport_id=1");
+		client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
+		try {
+			HttpResponse response = client.execute(get);
+			System.out.print(response.toString() );
+			InputStream istream = response.getEntity().getContent();
+			String result = IOUtils.toString(istream);
+			System.out.print(result);
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@Test()
 	public void checkProxyPost() {
 		httpService.setTimeBoundaryCheckinServiceProperty(5 * 24 * 60l);
 	}
+	
+	
 }
