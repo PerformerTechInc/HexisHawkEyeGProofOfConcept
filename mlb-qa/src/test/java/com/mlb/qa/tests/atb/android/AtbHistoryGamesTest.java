bfadddd3647fcbe325f89d1d462b1101740f61a5
administrator <administrator@50.18.225.38.com>
2014-06-10 17:52:06 -0700

    Update AtbHistoryGamesTest.java

--- mlb-qa/src/test/java/com/mlb/qa/tests/atb/android/AtbHistoryGamesTest.java
+++ mlb-qa/src/test/java/com/mlb/qa/tests/atb/android/AtbHistoryGamesTest.java
@@ -1,20 +1,5 @@
 package com.mlb.qa.tests.atb.android;
 
-import java.util.List;
-import java.util.Random;
-
-import com.qaprosoft.carina.core.foundation.log.TestLogCollector;
-import com.qaprosoft.carina.core.foundation.webdriver.Screenshot;
-import org.jfree.util.Log;
-import org.joda.time.DateTime;
-import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-import org.testng.ITestContext;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
 import com.mlb.qa.atb.AtbParameter;
 import com.mlb.qa.atb.android.page.AtbAndroidPage;
 import com.mlb.qa.atb.android.page.history.AtbAddGamePage;
@@ -24,127 +9,119 @@ import com.mlb.qa.atb.model.game.Game;
 import com.mlb.qa.atb.service.authorization.AuthorizationService;
 import com.mlb.qa.atb.service.lookup.AtbLookupService;
 import com.qaprosoft.carina.core.foundation.UITest;
-import com.qaprosoft.carina.core.foundation.webdriver.DriverFactory;
+import org.joda.time.DateTime;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Random;
 
 public class AtbHistoryGamesTest extends UITest {
 
-	private AuthorizationService authorizationService;
-	private WebDriver prepDriver;
+    private AuthorizationService authorizationService;
 
-	@DataProvider(name = "prepareTestData")
-	public Object[][] prepareTestData(ITestContext context) {
-		Object[][] allArguments = createTestArgSets(context, "Execute", "y");
-		Random rn = new Random();
-		int i = rn.nextInt(allArguments.length);
-		Object[][] a = { allArguments[i] };
-		return a;
-	}
-	
-	@BeforeClass
-	public void changeUser() {
-		prepDriver = DriverFactory.create("temp");
-		authorizationService = new AuthorizationService(prepDriver);
-		authorizationService.reloginUser(
-				AtbParameter.MLB_ATB_ADITIONAL_USER.getValue(),
-				AtbParameter.MLB_ATB_ADITIONAL_PASSWORD.getValue());
-		prepDriver.quit();
-	}
+    @DataProvider(name = "prepareTestData")
+    public Object[][] prepareTestData(ITestContext context) {
+        Object[][] allArguments = createTestArgSets(context, "Execute", "y");
+        Random rn = new Random();
+        int i = rn.nextInt(allArguments.length);
+        Object[][] a = {allArguments[i]};
+        return a;
+    }
 
-	@Test(dataProvider = "prepareTestData", description = "Add game from the future")
-	public void checkAddFutureGame(String teamAbbrev) {
-        authorizationService = new AuthorizationService(prepDriver);
+
+    @Test(dataProvider = "prepareTestData", description = "Add game from the future", priority = 1)
+    public void checkAddFutureGame(String teamAbbrev) {
+        authorizationService = new AuthorizationService(getDriver());
         authorizationService.reloginUser(
                 AtbParameter.MLB_ATB_ADITIONAL_USER.getValue(),
                 AtbParameter.MLB_ATB_ADITIONAL_PASSWORD.getValue());
-		AtbLookupService lookupService = new AtbLookupService();
-		String year = AtbParameter.MLB_ATB_SEASON.getValue();
-		int month = new DateTime().getMonthOfYear() + 1;
-		Team team = lookupService.lookupTeamByAbbrev(teamAbbrev, year);
-		List<Game> gamesBe = lookupService.lookupListOfGamesForTheMonth(
-				team.getTeamId(), Integer.parseInt(year), month);
+        AtbLookupService lookupService = new AtbLookupService();
+        String year = AtbParameter.MLB_ATB_SEASON.getValue();
+        int month = new DateTime().getMonthOfYear() + 1;
+        Team team = lookupService.lookupTeamByAbbrev(teamAbbrev, year);
+        List<Game> gamesBe = lookupService.lookupListOfGamesForTheMonth(
+                team.getTeamId(), Integer.parseInt(year), month);
 
-		AtbHistoryMainPage atbHistoryMainPage = new AtbHistoryMainPage(
-				getDriver());
-		atbHistoryMainPage.clickOnMenuItem(AtbAndroidPage.Menu.HISTORY);
-		Assert.assertTrue(atbHistoryMainPage.isOpened(), "History isn't opened");
-		AtbAddGamePage atbAddGamePage = atbHistoryMainPage.getAtbAddGamePage();
-		Assert.assertTrue(atbAddGamePage.isOpened(), "Add Game isn't opened");
-		atbAddGamePage.selectHomeTeam(gamesBe.get(gamesBe.size() - 1)
-				.getHomeTeamFull());
-		atbAddGamePage.selectAwayTeam(gamesBe.get(gamesBe.size() - 1)
-				.getAwayTeamFull());
-		atbAddGamePage.selectYear(year);
-		atbAddGamePage.selectMonth(gamesBe.get(gamesBe.size() - 1)
-				.getGameDate().monthOfYear().getAsText());
-		atbAddGamePage.selectDay(((Integer) gamesBe.get(gamesBe.size() - 1)
-				.getGameDate().getDayOfMonth()).toString());
-		atbAddGamePage.clickSearchAmdSelectGame();
-		atbAddGamePage.clickAddDeleteButton();
-		Assert.assertTrue(atbAddGamePage.checkErrorText());
-
-	}
-
-	
-	@Test(dataProvider = "prepareTestData", description = "Add game from the past")
-	public void checkAddPastGame(String teamAbbrev) {
-        authorizationService = new AuthorizationService(prepDriver);
-        authorizationService.reloginUser(
-                AtbParameter.MLB_ATB_ADITIONAL_USER.getValue(),
-                AtbParameter.MLB_ATB_ADITIONAL_PASSWORD.getValue());
-		AtbLookupService lookupService = new AtbLookupService();
-		String year = AtbParameter.MLB_ATB_SEASON.getValue();
-		int month = new DateTime().getMonthOfYear() - 1;
-		Team team = lookupService.lookupTeamByAbbrev(teamAbbrev, year);
-		List<Game> gamesBe = lookupService.lookupListOfGamesForTheMonth(
-				team.getTeamId(), Integer.parseInt(year), month);
-
-		AtbHistoryMainPage atbHistoryMainPage = new AtbHistoryMainPage(
-				getDriver());
-		atbHistoryMainPage.clickOnMenuItem(AtbAndroidPage.Menu.HISTORY);
-		Assert.assertTrue(atbHistoryMainPage.isOpened(), "History isn't opened");
-		AtbAddGamePage atbAddGamePage = atbHistoryMainPage.getAtbAddGamePage();
-		Assert.assertTrue(atbAddGamePage.isOpened(), "Add Game isn't opened");
-		atbAddGamePage.selectHomeTeam(gamesBe.get(gamesBe.size() - 1)
-                .getHomeTeamFull());
-		atbAddGamePage.selectAwayTeam(gamesBe.get(gamesBe.size() - 1)
-                .getAwayTeamFull());
-		atbAddGamePage.selectYear(year);
-		atbAddGamePage.selectMonth(gamesBe.get(gamesBe.size() - 1)
-                .getGameDate().monthOfYear().getAsText());
-		atbAddGamePage.selectDay(((Integer) gamesBe.get(gamesBe.size() - 1)
-                .getGameDate().getDayOfMonth()).toString());
-		atbAddGamePage.clickSearchAmdSelectGame();
-		atbAddGamePage.clickAddDeleteButton();
-        Assert.assertTrue(atbAddGamePage.checkSuccessText());
-
-	}
-
-    @Test(description = "Delete game from history")
-    public void deleteGameTest(){
-        authorizationService = new AuthorizationService(prepDriver);
-        authorizationService.reloginUser(
-                AtbParameter.MLB_ATB_ADITIONAL_USER.getValue(),
-                AtbParameter.MLB_ATB_ADITIONAL_PASSWORD.getValue());
-        TestLogCollector.addScreenshotComment(Screenshot.capture(getDriver(), true), "TEST FAILED - 1");
         AtbHistoryMainPage atbHistoryMainPage = new AtbHistoryMainPage(
                 getDriver());
-        TestLogCollector.addScreenshotComment(Screenshot.capture(getDriver(), true), "TEST FAILED - 2");
         atbHistoryMainPage.clickOnMenuItem(AtbAndroidPage.Menu.HISTORY);
-        atbHistoryMainPage.getRandomEvent();
-        AtbAddGamePage atbAddGamePage = new AtbAddGamePage(getDriver());
-        atbAddGamePage.clickDeleteChekIn();
+        Assert.assertTrue(atbHistoryMainPage.isOpened(), "History isn't opened");
+        AtbAddGamePage atbAddGamePage = atbHistoryMainPage.getAtbAddGamePage();
+        Assert.assertTrue(atbAddGamePage.isOpened(), "Add Game isn't opened");
+        atbAddGamePage.selectHomeTeam(gamesBe.get(gamesBe.size() - 1)
+                .getHomeTeamFull());
+        atbAddGamePage.selectAwayTeam(gamesBe.get(gamesBe.size() - 1)
+                .getAwayTeamFull());
+        atbAddGamePage.selectYear(year);
+        atbAddGamePage.selectMonth(gamesBe.get(gamesBe.size() - 1)
+                .getGameDate().monthOfYear().getAsText());
+        atbAddGamePage.selectDay(((Integer) gamesBe.get(gamesBe.size() - 1)
+                .getGameDate().getDayOfMonth()).toString());
+        atbAddGamePage.clickSearchAmdSelectGame();
+        atbAddGamePage.clickAddDeleteButton();
+        Assert.assertTrue(atbAddGamePage.checkErrorText());
+
+    }
+
+
+    @Test(dataProvider = "prepareTestData", description = "Add game from the past", priority = 2)
+    public void checkAddPastGame(String teamAbbrev) {
+        authorizationService = new AuthorizationService(getDriver());
+        authorizationService.reloginUser(
+                AtbParameter.MLB_ATB_ADITIONAL_USER.getValue(),
+                AtbParameter.MLB_ATB_ADITIONAL_PASSWORD.getValue());
+        AtbLookupService lookupService = new AtbLookupService();
+        String year = AtbParameter.MLB_ATB_SEASON.getValue();
+        int month = new DateTime().getMonthOfYear() - 1;
+        Team team = lookupService.lookupTeamByAbbrev(teamAbbrev, year);
+        List<Game> gamesBe = lookupService.lookupListOfGamesForTheMonth(
+                team.getTeamId(), Integer.parseInt(year), month);
+
+        AtbHistoryMainPage atbHistoryMainPage = new AtbHistoryMainPage(
+                getDriver());
+        atbHistoryMainPage.clickOnMenuItem(AtbAndroidPage.Menu.HISTORY);
+        Assert.assertTrue(atbHistoryMainPage.isOpened(), "History isn't opened");
+        AtbAddGamePage atbAddGamePage = atbHistoryMainPage.getAtbAddGamePage();
+        Assert.assertTrue(atbAddGamePage.isOpened(), "Add Game isn't opened");
+        atbAddGamePage.selectHomeTeam(gamesBe.get(gamesBe.size() - 1)
+                .getHomeTeamFull());
+        atbAddGamePage.selectAwayTeam(gamesBe.get(gamesBe.size() - 1)
+                .getAwayTeamFull());
+        atbAddGamePage.selectYear(year);
+        atbAddGamePage.selectMonth(gamesBe.get(gamesBe.size() - 1)
+                .getGameDate().monthOfYear().getAsText());
+        atbAddGamePage.selectDay(((Integer) gamesBe.get(gamesBe.size() - 1)
+                .getGameDate().getDayOfMonth()).toString());
+        atbAddGamePage.clickSearchAmdSelectGame();
         atbAddGamePage.clickAddDeleteButton();
         Assert.assertTrue(atbAddGamePage.checkSuccessText());
 
     }
 
-	@AfterClass
-	public void returnDefaultUser() {
-		prepDriver = DriverFactory.create("temp");
-		authorizationService = new AuthorizationService(prepDriver);
-		authorizationService.reloginUser(
-				AtbParameter.MLB_ATB_DEFAULT_USER.getValue(),
-				AtbParameter.MLB_ATB_DEFAULT_PASSWORD.getValue());
-		prepDriver.quit();
-	}
+    @Test(description = "Delete game from history", priority = 3)
+    public void deleteGameTest() {
+        try {
+            authorizationService = new AuthorizationService(getDriver());
+            authorizationService.reloginUser(
+                    AtbParameter.MLB_ATB_ADITIONAL_USER.getValue(),
+                    AtbParameter.MLB_ATB_ADITIONAL_PASSWORD.getValue());
+            AtbHistoryMainPage atbHistoryMainPage = new AtbHistoryMainPage(
+                    getDriver());
+            atbHistoryMainPage.clickOnMenuItem(AtbAndroidPage.Menu.HISTORY);
+            atbHistoryMainPage.getRandomEvent();
+            AtbAddGamePage atbAddGamePage = new AtbAddGamePage(getDriver());
+            atbAddGamePage.clickDeleteChekIn();
+            atbAddGamePage.clickAddDeleteButton();
+            Assert.assertTrue(atbAddGamePage.checkSuccessText());
+        } finally {
+            authorizationService = new AuthorizationService(getDriver());
+            authorizationService.reloginUser(
+                    AtbParameter.MLB_ATB_DEFAULT_USER.getValue(),
+                    AtbParameter.MLB_ATB_DEFAULT_PASSWORD.getValue());
+        }
+
+    }
 }
