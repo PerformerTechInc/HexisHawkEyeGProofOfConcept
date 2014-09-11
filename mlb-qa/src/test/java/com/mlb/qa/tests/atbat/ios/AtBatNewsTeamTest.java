e8aad79dec07858b652148cd74a612affe133c96
ivayner <igor.vayner@mlb.com>
2014-09-11 17:09:21 -0400

    Update news tests and tests data

--- mlb-qa/src/test/java/com/mlb/qa/tests/atbat/ios/AtBatNewsTeamTest.java
+++ mlb-qa/src/test/java/com/mlb/qa/tests/atbat/ios/AtBatNewsTeamTest.java
@@ -4,12 +4,13 @@ import com.mlb.qa.at_bat.ios.page.authentication.AtBatLoginPage;
 import com.mlb.qa.at_bat.ios.page.common.AtBatStartPage;
 import com.mlb.qa.at_bat.ios.page.footer.AtBatBottomMenuPage;
 import com.mlb.qa.at_bat.ios.page.footer.AtBatMorePage;
-import com.mlb.qa.at_bat.ios.page.footer.AtBatNewsPage;
 import com.mlb.qa.at_bat.ios.page.team.AtBatTeamPage;
 import com.mlb.qa.at_bat.ios.page.team.AtBatTeamSelectionPage;
+import com.mlb.qa.at_bat.ios.page.team.news.AtBatNewsPage;
 import com.mlb.qa.atb.service.lookup.AtbLookupService;
 import com.qaprosoft.carina.core.foundation.UITest;
 import org.openqa.selenium.By;
+import org.openqa.selenium.browserlaunchers.Sleeper;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -19,20 +20,11 @@ import java.util.List;
 public class AtBatNewsTeamTest extends UITest {
 
 
-    @Parameters({"TUID", "team_name"})
+    @Parameters({"team_abbrev","TUID", "team_name"})
     @Test(dataProvider = "excel_ds")
-    public void checkNewsGlobal(String TUID, String team_name) {
-        AtbLookupService lookupService = new AtbLookupService();
-        List<String> news = lookupService.getNewsTitles("nym");
-        int i = 1;
-        for (String aNew : news) {
-            if (aNew != null) {
-                System.out.println(i + " " + aNew);
-                i++;
-            }
-        }
-
-
+    public void checkNewsGlobal(String team_abbrev, String TUID, String team_name) {
+        //AtbLookupService lookupService = new AtbLookupService();
+        //List<String> news = lookupService.getNewsTitles(team_abbrev.toLowerCase());
         AtBatStartPage atBatStartPage = new AtBatStartPage(driver);
         AtBatLoginPage atBatLoginPage = atBatStartPage.getAtBatLoginPage();
         atBatLoginPage.login();
@@ -42,19 +34,26 @@ public class AtBatNewsTeamTest extends UITest {
 
         AtBatTeamSelectionPage atBatTeamSelectionPage = atBatMorePage.getAtBatTeamSelectionPage();
         AtBatTeamPage atBatTeamPage = atBatTeamSelectionPage.selectTeam(TUID);
-        atBatTeamPage.getAtBatNewsPage();
+        AtBatNewsPage atBatNewsPage = atBatTeamPage.getAtBatNewsPage();
 
-        i = 1;
+        AtbLookupService lookupService = new AtbLookupService();
+        List<String> news = lookupService.getNewsTitles(team_abbrev.toLowerCase());
+
+        int i = 1;
         SoftAssert softAssert = new SoftAssert();
+        Sleeper.sleepTightInSeconds(7);
         for (String aNew : news) {
+
             if (aNew != null && !aNew.contains("'")) {
                 softAssert.assertTrue(driver.findElements(By.name(aNew)).size() > 0, "Post with title '" + aNew + "' not present");
             }
+            System.out.println("Iteration: " + i);
             i++;
-            if (i == 30) {
+            if (i == 10) {
                 break;
             }
         }
+        atBatNewsPage.backToPrevious();
         softAssert.assertAll();
 
 
