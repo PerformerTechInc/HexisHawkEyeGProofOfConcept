519229506b38634c15c7690de9e3895d950ffb64
administrator <administrator@50.18.225.38.com>
2014-10-09 06:59:38 -0700

    small adjustments for BallparkMusicJson.java

--- mlb-qa/src/main/java/com/mlb/qa/atb/model/music/json/BallparkMusicJson.java
+++ mlb-qa/src/main/java/com/mlb/qa/atb/model/music/json/BallparkMusicJson.java
@@ -1,29 +1,28 @@
 package com.mlb.qa.atb.model.music.json;
 
-    import java.io.ByteArrayInputStream;
-    import java.io.FileNotFoundException;
-    import java.io.InputStream;
-    import java.nio.charset.StandardCharsets;
-    import java.util.HashMap;
-    import java.util.LinkedList;
-    import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
-    import javax.xml.bind.JAXBContext;
-    import javax.xml.bind.JAXBException;
-    import javax.xml.bind.Unmarshaller;
-    import javax.xml.transform.stream.StreamSource;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
-    import com.mlb.qa.common.http.HttpHelper;
-    import com.mlb.qa.common.http.HttpResult;
-    import com.qaprosoft.carina.core.foundation.http.HttpClient;
-    import org.eclipse.persistence.jaxb.UnmarshallerProperties;
-    import org.eclipse.persistence.oxm.MediaType;
-    import org.eclipse.persistence.oxm.annotations.XmlPath;
+import org.eclipse.persistence.jaxb.UnmarshallerProperties;
+import org.eclipse.persistence.oxm.MediaType;
+import org.eclipse.persistence.oxm.annotations.XmlPath;
 
-    import com.mlb.qa.atb.model.Item;
-    import com.mlb.qa.atb.model.music.Music;
-    import com.mlb.qa.atb.model.music.MusicCategory;
-    import com.mlb.qa.common.exception.TestRuntimeException;
+import com.mlb.qa.atb.model.Item;
+import com.mlb.qa.atb.model.music.Music;
+import com.mlb.qa.atb.model.music.MusicCategory;
+import com.mlb.qa.common.exception.TestRuntimeException;
+import com.mlb.qa.common.http.HttpHelper;
+import com.mlb.qa.common.http.HttpResult;
 
     public class BallparkMusicJson implements Item {
 
